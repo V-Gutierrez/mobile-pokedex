aab405b92ac85ce90c8ee16807f0f167fb745893
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2021-09-21 19:02:45 -0400

    Chore: bootstrap project

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native'
+
+
+
+const styles = StyleSheet.create({})
+
+const App = () => {
+  return (
+    <SafeAreaView >
+      <StatusBar />
+      <Text>Template</Text>
+    </SafeAreaView>
+  )
+}
+
+
+export default App
