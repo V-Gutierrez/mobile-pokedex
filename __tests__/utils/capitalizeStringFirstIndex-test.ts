646f237f90c6b163deffbafe0952377afad9ce4c
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2021-09-22 10:00:01 -0400

    implement capitalize method

---
+++ __tests__/utils/capitalizeStringFirstIndex-test.ts
@@ -0,0 +1,14 @@
+import { capitalizeStringFirstIndex } from './../../src/utils/capitalizeStringFirstIndex'
+
+
+describe('capitalizeStringFirstIndex util', () => {
+  it('should capitalize the first letter of a string', () => {
+
+    const sample = 'pokedex'
+
+    const capitalizedSample = capitalizeStringFirstIndex(sample)
+
+    expect(capitalizedSample).toStrictEqual('Pokedex')
+
+  });
+});
