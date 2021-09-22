efca66fe8f7725ddc1b0dc0e1d2632009fa74876
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2021-09-22 10:31:57 -0400

    Implement parser for assets url

---
+++ __tests__/utils/parseAssetUrl-test.ts
@@ -0,0 +1,12 @@
+import { parseAssetUrl } from 'utils';
+
+describe('parseAssetUrl utils', () => {
+it('should return a url leading to improved pokemon asset', () => {
+  const id = 1
+
+  const parse = parseAssetUrl('https://pokeapi.co/api/v2/pokemon/1')
+
+
+  expect(parse).toStrictEqual(`https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png`)
+  })
+})
--- src/utils/parseAssetUrl.ts
+++ src/utils/parseAssetUrl.ts
@@ -1,3 +1,19 @@
-export const parseAssetUrl = (id: number): string => {
-  return 'id'
+/*
+* @params: {url: string} as POKEAPI url
+*/
+
+export const parseAssetUrl = (url: string): string => {
+  const id = url.split('/').filter(item => !isNaN(Number(item))).filter(item => item !== '')[0]
+
+  const parseID = () => {
+  if (Number(id) < 10) {
+      return `00${Number(id)}`;
+    } else if (Number(id) >= 10 && Number(id) < 100) {
+      return `0${Number(id)}`;
+    } else if (Number(id) > 99) {
+      return id
+    }
+  }
+
+  return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${parseID()}.png`
 }
